188fa94f0519569ef6a423adcab69d0f7a439cc7
Stephen Lie Juliando Soetopo <SS37685X@cimbniaga.co.id>
2022-03-27 20:13:24 +0700

    Add dependency in pom.xml for h2 database

--- src/main/java/com/myproject/usermanagementservice/web/UserController.java
+++ src/main/java/com/myproject/usermanagementservice/web/UserController.java
@@ -10,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/api/user")
