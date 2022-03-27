6523b1e7ade0d5a7e4d438229448bca614bd3eec
Stephen Lie Juliando Soetopo <SS37685X@cimbniaga.co.id>
2022-03-27 18:22:05 +0700

    Update user data controller

--- src/main/java/com/myproject/usermanagementservice/web/UserController.java
+++ src/main/java/com/myproject/usermanagementservice/web/UserController.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/api/user")
@@ -28,9 +33,27 @@ public class UserController {
     @ApiOperation(value = "Service to get UserProfile")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/{userprofileid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{userDataId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDataResponse getUserData(@PathVariable Long userDataId) {
         log.info("In method getUserData with parameter: userDataId: {}", userDataId);
         return userDataService.getUserData(userDataId);
+    }
+
+    @ApiOperation(value = "Service to update UserProfile")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/{idToUpdate}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDataResponse updateUserData(@RequestBody(required = false) UserData userData, @PathVariable Long idToUpdate,
+                                          HttpServletRequest request, HttpServletResponse response) throws Exception {
+        log.info("In method updateUserData with parameters: UserData {} and user ID: {}", userData, idToUpdate);
+
+        UserDataResponse userDataResponse = userDataService.getUserData(idToUpdate);
+
+        if ("Failed".equalsIgnoreCase(userDataResponse.getStatus())) {
+            throw new Exception();
+        }
+
+        return userDataService.saveOrUpdateUserData(userData);
+
     }
 }
